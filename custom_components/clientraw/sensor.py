ba6e1cad8edd1e78b93d384981ba518134941ae5
Pavel Slama <info@pavelslama.cz>
2022-03-20 19:41:45 +0100

    bump version

--- custom_components/clientraw/sensor.py
+++ custom_components/clientraw/sensor.py
@@ -24,7 +24,7 @@ from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.event import (async_track_utc_time_change,
                                          async_call_later)
 
-__version__ = '2.2.2'
+__version__ = '2.2.3'
 
 _LOGGER = logging.getLogger(__name__)
 
